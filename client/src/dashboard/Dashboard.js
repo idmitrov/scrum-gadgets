5740ea8680398104f1f18128a91ddb99f2dc0910
Dimitar <mailto.dimitrov@gmail.com>
2018-07-01 00:10:51 +0300

    Separate public and private parts of layout

--- client/src/dashboard/Dashboard.js
+++ client/src/dashboard/Dashboard.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 const Dashboard = (props) => (
     <section>
       <h2>Dashboard</h2>
 
-      {/*
-        TODO: Make the links grid items
-        with nice banners related to each tool/grid item
-      */}
-      <div>
-        <Link to="/tool/daily" title="Daily">Daily</Link>
-      </div>
-      <div>
-        <Link to="/tool/poker" title="poker">Poker</Link>
-      </div>
-      <div>
-        <Link to="/tool/retrospective" title="Retrospective">Retrospective</Link>
-      </div>
+      {/* TODO: Extract public part into Home and do logic in routes */}
+      {
+        props.authenticated ? (
+        // TODO: Make the links grid items
+        // with nice banners related to each tool/grid item
+        <div>
+          <div>
+            <Link to="/tool/daily" title="Daily">Daily</Link>
+          </div>
+
+          <div>
+            <Link to="/tool/poker" title="poker">Poker</Link>
+          </div>
+
+          <div>
+            <Link to="/tool/retrospective" title="Retrospective">Retrospective</Link>
+          </div>
+        </div>
+        ) : (
+          <p>Welcome to Scrum Gadgets</p>
+        )
+      }
     </section>
 );
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    authenticated: state.auth.authenticated
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
