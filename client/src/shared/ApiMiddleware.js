c6a4d6f7fcac0a6952cc00735d81cac2042e762f
Dimitar <mailto.dimitrov@gmail.com>
2018-07-18 00:44:48 +0300

    Improve ApiMiddleware by wrapping fetch() in promise and use resolve/reject, dispatch validation action on error

--- client/src/shared/ApiMiddleware.js
+++ client/src/shared/ApiMiddleware.js
@@ -7,7 +7,6 @@ const { host, port } = AppConfig[env].api;
 
 export const ApiMiddleware = (store) => (next) => (action) => {
   if (action.meta && action.meta.api) {
-    // TODO: Replace it with fetch() and real API endpoints
     let options = {
       method: action.payload.method,
       headers: {
@@ -20,54 +19,55 @@ export const ApiMiddleware = (store) => (next) => (action) => {
       options.body = JSON.stringify(action.payload.data);
     }
 
-    return fetch(`//${host}:${port}/api${action.payload.url}`, options)
-      .then((response) => {
-        if (!response.ok) {
-          Promise.reject(response);
-        } else {
-          const contentType = response.headers.get('Content-Type');
+    return new Promise((resolve, reject) => {
+      // TODO: Check for loading meta and
+      // if is set dispatch LOADING action,
+      // to prevent user interaction during fetching data
+      fetch(`//${host}:${port}/api${action.payload.url}`, options)
+        .then((response) => {
+          if (!response.ok) {
+            throw response;
+          }
 
-          if (/application\/json/.test(contentType)) {
+          if (/application\/json/.test(response.headers.get('Content-Type'))) {
             return response.json();
           }
 
           return response.text();
-        }
-      })
-      .then((response) => {
-        if (response.errors && response.errors.length) {
-          store.dispatch(sharedActions.setNotifications(response.errors));
+        })
+        .then((response) => {
+          if (response.errors && response.errors.length) {
+            throw response;
+          }
 
-          Promise.reject(response.errors);
-        } else if (response.errors && !response.errors.length) {
-          Promise.resolve(store.dispatch({
+          resolve(store.dispatch({
             type: action.success,
             payload: response.data
           }));
-        }
-      })
-      .catch((response) => {
-        if (!response) {
-          // TODO: Handle Unexpected Errors
-          console.log(`Unexpected error: ${response}`);
-        } else {
-          if (response.status) {
-            // TODO: Handle HTTP Errors
-            switch (response.status) {
-              case 404:
-                console.log(`Error 404: ${response.statusText}`);
-                break;
-              case 500:
-                console.log(`Error 500: ${response.statusText}`);
-                break;
-              default:
-                console.log(`Something went wrong ${response.statusText}`);
+        })
+        .catch((response) => {
+          reject(response);
+
+          // TODO: REPLACE CONSOLE WITH LOGGER
+          if (!response) {
+            console.log(`Unexpected error: ${response}`);
+          } else {
+            if (response.status) {
+              switch (response.status) {
+                case 404:
+                  console.log(`Error 404: ${response.statusText}`);
+                  break;
+                case 500:
+                  console.log(`Error 500: ${response.statusText}`);
+                  break;
+                default:
+                  console.log(`Something went wrong ${response.statusText}`);
+              }
+            } else if (response.errors) {
+              store.dispatch(sharedActions.setNotifications(response.errors));
             }
-          } else if (response.errors) {
-            // TODO: Handle validation errors
-            console.log(response.errors);
           }
-        }
+        });
       });
   }
 
